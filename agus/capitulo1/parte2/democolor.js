680e2ce76b32d6b0d89d957c2dfc896a7d6d5611
Matheus <matheusmpessoa17@gmail.com>
2016-11-28 21:37:08 -0200

    Cap1 parte2 agus resolvido

---
+++ agus/capitulo1/parte2/democolor.js
@@ -0,0 +1,4 @@
+var clc = require('cli-color');
+console.log(clc.blue('"Hello node.js" in blue'));
+console.log(clc.red('"Hello node.js" in red'));
+console.log(clc.green('"Hello node.js" in green'));
