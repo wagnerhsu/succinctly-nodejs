0c179bcc8153dc9e630e83f4872250083a0aa9e7
Wagner Xu <wagnerhsu@hotmail.com>
2020-05-25 21:30:42 +0800

    Add BasicWebApp

---
+++ MyCode/BasicWebApp/README.md
@@ -0,0 +1,3 @@
+## 2020-05-25
+- Use Express to start static web
+- Basic html to test bootstrap
---
+++ MyCode/BasicWebApp/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('.'));
+app.listen(8000,()=> {
+    console.log('Listening on port 8000');
+});
